64bf7dbab3a9a303fbab8c3d43209275464c177d
chasevara <137222733+chasevara@users.noreply.github.com>
2023-12-03 17:18:48 -0800

    "DialogEngine updated to parse room number text into ints; output from response() function still buggy due to ductionary value unpacking"

--- DialogEngine.py
+++ DialogEngine.py
@@ -3,6 +3,8 @@
 
 # dependencies: 
 import csv
+import re
+from word2number import w2n
 
 class DialogEngine:
 
@@ -22,43 +24,45 @@ class DialogEngine:
     self.syn_dict = self.build_syn_dict(input_syn_file_name)
     self.resp_dict = self.build_response_dict(input_response_file_name)
 
-  # build {synonym : input_prompt} dictionary from .txt file containing input-synonyms
-  # dictionary data, each line formatted as "<prompt> <input_syn1 input_syn2..>"
-  def build_syn_dict(cls, input_syn_file_name):
-    syn_dict = {}
-    with open(input_syn_file_name, mode='r', newline='', encoding='utf-8') as csvfile:
-      csvreader = csv.reader(csvfile)
-      for row in csvreader:
-        if len(row) >= 2:
-          # The first item is the prompt and the rest are synonyms
-          prompt = row[0].strip()
-          synonyms = [syn.strip() for syn in row[1:]]
-          for syn in synonyms:
-            syn_dict[syn] = prompt
-    return syn_dict
-
   # build {input_prompt : response} dictionary from .txt file containing response
   # dictionary, each line formatted as "<input_prompt> <response>"
   def build_response_dict(cls, input_response_file_name):
     resp_dict = {}
     with open(input_response_file_name, mode='r', newline='', encoding='utf-8') as csvfile:
-      csvreader = csv.reader(csvfile)
+      csvreader = csv.reader(csvfile, delimiter='|')
       for row in csvreader:
         if len(row) >= 3:
+          print(row)
           # Assuming the first column is the prompt and the second is the response
           prompt = row[0].strip()
           response = row[1].strip()
-          actions = row[2:]
-          resp_dict[prompt] = (response, actions)
+          actions = [int(num) for num in row[2:]] # convert string nums read from .csv to ints
+          resp_dict[prompt] = response, actions
     return resp_dict
 
+  # build {synonym : input_prompt} dictionary from .txt file containing input-synonyms
+  # dictionary data, each line formatted as "<prompt> <input_syn1 input_syn2..>"
+  def build_syn_dict(cls, input_syn_file_name):
+    syn_dict = {}
+    with open(input_syn_file_name, mode='r', newline='', encoding='utf-8') as csvfile:
+      csvreader = csv.reader(csvfile, delimiter='|')
+      for row in csvreader:
+        if len(row) >= 2:
+          # The first item is the prompt and the rest are synonyms
+          print(row)
+          prompt = row[0].strip()
+          synonyms = [syn.strip() for syn in row[1:]]
+          for syn in synonyms:
+            syn_dict[syn] = prompt
+    return syn_dict
+  
   ################################################################################################# 
   ################ functions to generate data text files from object dictionaries ################# 
 
   # function to save prompts and their responses to .csv file called by stored syn_file_name
   def responses_to_file(self):
     with open(self.resp_file_name, mode='w', newline='', encoding='utf-8') as csv_file:
-      writer = csv.writer(csv_file)
+      writer = csv.writer(csv_file, delimiter='|')
 
       for prompt in self.resp_dict:
           # Create a row with the prompt followed by its synonyms
@@ -69,7 +73,7 @@ class DialogEngine:
   # function to save prompts and their synonyms to a different .csv file called by input name
   def responses_to_new_file(self, output_resp_file_name):
     with open(output_resp_file_name, mode='w', newline='', encoding='utf-8') as csv_file:
-      writer = csv.writer(csv_file)
+      writer = csv.writer(csv_file, delimiter='|')
 
       for prompt in self.resp_dict:
           # Create a row with the prompt followed by its synonyms
@@ -83,7 +87,7 @@ class DialogEngine:
     prompt_syns = _invert_str_dict(self.syn_dict)
 
     with open(self.syn_file_name, mode='w', newline='', encoding='utf-8') as csv_file:
-      writer = csv.writer(csv_file)
+      writer = csv.writer(csv_file, delimiter='|')
 
       for prompt in prompt_syns:
           # Create a row with the prompt followed by its synonyms
@@ -96,7 +100,7 @@ class DialogEngine:
     prompt_syns = _invert_str_dict(self.syn_dict)
 
     with open(output_syn_file_name, mode='w', newline='', encoding='utf-8') as csv_file:
-      writer = csv.writer(csv_file)
+      writer = csv.writer(csv_file, delimiter='|')
 
       for prompt in prompt_syns:
           # Create a row with the prompt followed by its synonyms
@@ -104,27 +108,91 @@ class DialogEngine:
           writer.writerow(row)
 
   ################################################################################################# 
+  ### functions to parse input for (prompts, numbers) to use with object and give output ##########
+  
+  # parses a string for a three digit number. if a three digit number is not found, returns none
+  def parse_number_words(self, input_string):
+    # Replace 'oh' or 'o' with 'zero' for clarity in parsing
+    input_string = input_string.replace(" oh ", " zero ").replace(" o ", " zero ")
+
+    # Regular expression to find number words
+    number_word_pattern = re.compile(r"\b(one|two|three|four|five|six|seven|eight|nine|zero|ten|"
+                                     r"eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen|"
+                                     r"eighteen|nineteen|twenty|thirty|forty|fifty|sixty|seventy|"
+                                     r"eighty|ninety|hundred|thousand)\b", re.IGNORECASE)
+
+    number_words = number_word_pattern.findall(input_string)
+
+    # Check if the number of words is between 2 and 3
+    if len(number_words) < 2 or len(number_words) > 3:
+      return None
+
+    try:
+      # Parse the hundreds place
+      hundreds = w2n.word_to_num(number_words[0]) * 100
+
+      # Parse the tens and ones places
+      tens_ones = w2n.word_to_num(' '.join(number_words[1:])) if len(number_words) > 1 else 0
+
+      # Combine the values to form a three-digit number
+      three_digit_number = hundreds + tens_ones
+
+      # Check if the number is indeed a three-digit number
+      if 100 <= three_digit_number <= 999:
+        return three_digit_number
+      else:
+        return None
+    except ValueError:
+      return None
+  
+  def parse_for_prompt(self, input_string, response_dict):
+    found_prompt_list = [prompt for prompt in response_dict if prompt in input_string]
+    if len(found_prompt_list) != 1:
+      return None
+    else: 
+      return found_prompt_list[0]
+
+  ################################################################################################# 
   ################## functions to interact with the response object ###############################
 
-  # returns response for prompt word. 
+  # Return format: (<response_string>, <response_actions_ints_list>, <response_int>)
+  # Returns response for prompt phrase and any int number in found phrase
   # If prompt word is not mapped to a response, checks synonyms 
   # else, eturns "unkn"
+  # returns None for number if no number found
   def response(self, phrase):
-    response = self.resp_dict.get(phrase)
-    if response is None:
+    number = self.parse_number_words(phrase)
+    #TODO debug line
+    print("number found: ", number)
+    
+    # parse input against possible prompts
+    prompt = self.parse_for_prompt(phrase, self.resp_dict)
+    
+    # if prompt not found in phrase, parse against synonyms for prompts
+    if prompt == None:
+      prompt = self.parse_for_prompt(phrase, self.syn_dict)
+      
+    #TODO debug line
+    print("prompt found: ", prompt) 
+    
+    # If synonym still not found, return None, otherwise convert synonym to corresponding prompt
+    if prompt == None:
+      return "unkn", None, None
+    else:
       prompt = self.syn_dict.get(phrase)
-      if prompt is None:
-        return "unkn"
-      else:
-        return self.resp_dict.get(prompt)
-    return response
+      
+    # Get response
+    response, actions = self.resp_dict.get(phrase)
   
+    # Return tuple of response string, response actions ints lists, and response parsed number int
+    return response, actions, number
+    
   # prints object's prompts and associated responses
   def print_responses(self):
     for prompt in self.resp_dict:
       response, actions = self.resp_dict.get(prompt)
       print("(prompt) "+prompt+" | (response) "+response," | (actions sequence", actions)
-    return
+    return 
 
   # prints object's prompts and their synonyms
   def print_synonyms(self):
@@ -139,7 +207,7 @@ class DialogEngine:
       print(string)
   
   ################################################################################################# 
-  ######## functions to update response object information (prompt, synonyms, responses) #########
+  ######## functions to update response object information (prompt, synonyms, responses) ##########
 
   # add prompt, associated response, and a list of prompt synonyms
   # if the prompt word already exists, prints "prompt already associated, no action taken"
@@ -149,7 +217,7 @@ class DialogEngine:
       print("prompt {prompt} already associated, no action taken")
       return
     else:
-      self.resp_dict[prompt] = (response, list_synonyms)
+      self.resp_dict[prompt] = (response, list_actions)
       for syn in list_synonyms:
         self.add_synonym(prompt, syn)
       return
